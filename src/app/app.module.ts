eeaf02544ec8feb4ee31177a7626170e89c97116
irishSaltWater <irishsaltwater@gmail.com>
2021-01-17 12:53:03 +0000

    Added material dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppComponent } from './app.component';
 import { SarcasticTextComponent } from './sarcastic-text/sarcastic-text.component';
 import { FormsModule } from '@angular/forms';
 import { SarcasmPipe } from './sarcasm.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+
+
 
 @NgModule({
   declarations: [
@@ -14,9 +19,13 @@ import { SarcasmPipe } from './sarcasm.pipe';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
